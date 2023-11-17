150c817be4f422fdff86052136bad5cc5043789c
Disha Agarwal <68860743+dishamagarwal@users.noreply.github.com>
2023-11-17 14:11:25 -0500

    making the blackjack game

---
+++ Blackjack/Game.py
@@ -0,0 +1,129 @@
+############### Blackjack Project #####################
+
+#Difficulty Normal 😎: Use all Hints below to complete the project.
+#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
+#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
+#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
+
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size. 
+## There are no jokers. 
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
+##################### Hints #####################
+
+#Hint 1: Go to this website and try out the Blackjack game: 
+#   https://games.washingtonpost.com/games/blackjack/
+#Then try out the completed Blackjack project here: 
+#   http://blackjack-final.appbrewery.repl.run
+
+import random
+logo = """
+.------.            _     _            _    _            _    
+|A_  _ |.          | |   | |          | |  (_)          | |   
+|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
+| \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
+`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
+      |  \/ K|                            _/ |                
+      `------'                           |__/           
+      
+
+"""
+
+class Game:
+    player_points = 0
+    dealer_points = 0
+    player_hand = []
+    dealer_hand = []
+    rounds_played = 0
+    deck = {
+        'A': 11,
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8,
+        '9': 9,
+        'T': 10,
+        'J': 10,
+        'Q': 10,
+        'K': 10
+    }
+
+    def initial_deal(self):
+        self.dealer_hand = [random.choice(list(self.deck.keys())), random.choice(list(self.deck.keys()))]
+        self.dealer_points += self.deck[self.dealer_hand[0]] + self.deck[self.dealer_hand[1]]
+        self.player_hand = [random.choice(list(self.deck.keys())), random.choice(list(self.deck.keys()))]
+        self.player_points += self.deck[self.player_hand[0]] + self.deck[self.player_hand[1]]
+        self.rounds_played += 2
+        self.print_status()
+
+    def print_status(self):
+        print("Dealer's first card: " + self.dealer_hand[0])
+        print("Your cards: " + str(self.player_hand))
+        print("Current score: " + str(self.player_points))
+
+    def deal(self, deal):
+        self.rounds_played += 1
+        # if self.player_points > 21:
+        #     if 'A' in self.player_hand:
+        #             self.player_points -= 10
+        if deal == 'y' and self.rounds_played <= 4:
+            if self.dealer_points < 21 or self.player_points < 21:
+                card = random.choice(list(self.deck.keys()))
+                self.player_hand.append(card)
+                self.player_points += self.deck[card]
+            else:
+                print("Your total is already at max, cannot deal more cards. You need to go look up the rules")
+                return 'n'
+            self.print_status()
+        return deal
+    
+    def dealer_cards(self):
+        while self.dealer_points < 17:
+            card = random.choice(list(self.deck.keys()))
+            self.dealer_hand.append(card)
+            self.dealer_points += self.deck[card]
+
+    def show(self):
+        print("Your hand: " + str(self.player_hand))
+        print("Your current score: " + str(self.player_points))
+        print("Dealer's hand: " + str(self.dealer_hand))
+        print("Dealer's current score: " + str(self.dealer_points))
+        if self.player_points > 21 and self.dealer_points > 21:
+            print("Everyone's a bust losers")
+        elif self.player_points > 21:
+            print("You're bust loser, dealer wins")
+        elif self.dealer_points > 21:
+            print("Kkk you win!")
+        elif self.player_points == self.dealer_points:
+            print("Whatever, it's a draw")
+        elif self.player_points > self.dealer_points:
+            print("Aright, you win!")
+        else:
+            print("Dealer wins, you lose loser!")
+
+print(logo)
+play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+while play == 'y':
+    game = Game()
+    game.initial_deal()
+    game.dealer_cards()
+    deal = 'y'
+    while deal == 'y':
+        deal = input("Type 'y' to get another card, type 'n' to pass: ")
+        deal = game.deal(deal)
+    game.show()
+    play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+
+print("Thank you for playing! Bye.")
