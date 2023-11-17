e922837112e8c077c35fe0339573de29bc43e406
Disha Agarwal <68860743+dishamagarwal@users.noreply.github.com>
2023-11-16 19:17:57 -0500

    abstraction

--- CaesarCipher/Encryption.py
+++ CaesarCipher/Encryption.py
@@ -1,4 +1,22 @@
 class Encryption:
+    # abstraction: combined version of encode and decode
+    def caesar(self, text, shift, type):
+        coded = ""
+        bounds = 26
+        if type == "decode":
+            shift *= -1
+        else:
+            bounds *= -1
+        for ch in text:
+            asc = ord(ch)
+            if (97 <= asc and asc <= 122) or (65 <= asc and asc <= 90):
+                asc += shift
+                if (asc < 65) or asc > 122 or (asc > 90 and asc < 97):
+                    asc += bounds
+            coded += chr(asc)
+        return coded
+
+        return code
     def encode(self, s, shift) -> str:
         coded = ""
         for cur in s:
@@ -32,3 +50,6 @@ elif type == "decode":
     print(encryption.decode(word, shift))
 else:
     print("Incorrect input:(")
+# ALT version
+# if type == 'encode' or type == 'decode':
+#     print(encryption.caesar(word, shift, type))
