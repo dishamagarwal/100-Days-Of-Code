75e2d04d672fd6dee9044313fdc8319132241246
Disha Agarwal <68860743+dishamagarwal@users.noreply.github.com>
2023-12-09 12:36:47 -0500

    saving high score

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -9,6 +9,8 @@ class Scoreboard(Turtle):
         self.goto(0, 270)
         self.hideturtle()
         self.update_scoreboard()
+        with open("highscore.txt") as file:
+            self.high_score = int(file.read())
 
     def gain_point(self):
         self.score += 1
@@ -22,6 +24,11 @@ class Scoreboard(Turtle):
         self.write("Score: " + str(self.score), align="center", font=("Arial", 24, "normal"))
 
     def final_score(self):
+        high_score = 0
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("highscore.txt", mode="w") as file:
+                file.write(str(self.high_score))
         self.clear()
         self.goto(0, 0)
-        self.write("Game Over" + "\nFinal Score: " + str(self.score), align="center", font=("Arial", 24, "normal"))
+        self.write("Game Over" + "\nFinal Score: " + str(self.score) + "\nHigh Score: " + str(self.high_score), align="center", font=("Arial", 24, "normal"))
